93ab70c8961a564941b727ae22c2eff340dc3f37
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-02 23:00:50 +0530

    animare

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
+import toast, { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          style: {
+            background: "#333",
+            color: "#fff",
+            borderRadius: "100px",
+          },
+        }}
+      />
+    </>
+  );
 }
