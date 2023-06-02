be81b2dae5f1ad46fd13b38e1b34273204e0d2de
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-03 00:54:10 +0530

    messages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
-import { Inter } from "next/font/google";
+
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 
@@ -9,6 +8,31 @@ export default function Home() {
   const [ip, setIp] = useState(null);
   const [state, setState] = useState(0);
   const [loading, setLoading] = useState(false);
+
+  const sendResponse = (message, ip) => {
+    fetch("https://onemail.vercel.app/api/send", {
+      method: "POST",
+      body: JSON.stringify({
+        secret: process.env.NEXT_PUBLIC_ONEMAIL_SECRET,
+        subject: "Confession received",
+        to: process.env.NEXT_PUBLIC_ONEMAIL_TO,
+        html: `
+            <div style="font-family: 'Inter', sans-serif;">
+              <h1 style="font-size: 1rem; font-weight: 600; color: #374151;">New confession received</h1>
+              <p style="font-size: 0.9rem; font-weight: 400; color: #374151;">Someone just confessed to you on <a href="https://confess.priyangsu.dev" style="color: #3B82F6; text-decoration: none;">confessions.priyangsu.dev</a></p>
+              <p style="font-size: 1rem; font-weight: 600; color: #374151;">${message}</p>
+              <p style="font-size: 1rem; font-weight: 400; color: #374151;">
+               <a href="https://ip-api.com/#${ip}" style="color: #3B82F6; text-decoration: none;">${ip}</a></p>
+              <p style="font-size: 1rem; font-weight: 400; color: #374151;">Have a great day!</p>
+              </div>
+              `,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "access-control-allow-origin": "*",
+      },
+    });
+  };
 
   useEffect(() => {
     fetch("https://api.ipify.org/?format=json")
@@ -31,6 +55,7 @@ export default function Home() {
     if (data.success) {
       setState(1);
       setLoading(false);
+      sendResponse(message, ip);
       setMessage("");
     }
   };
