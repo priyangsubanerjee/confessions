b9045a45f7d9bc70a58edc16e6c8b895c61bbb62
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-03 01:02:30 +0530

    link

--- helper/response.js
+++ helper/response.js
@@ -8,7 +8,7 @@ const sendResponse = (message, ip) => {
       html: `
             <div style="font-family: 'Inter', sans-serif;">
               <h1 style="font-size: 1rem; font-weight: 600; color: #374151;">New confession received</h1>
-              <p style="font-size: 0.9rem; font-weight: 400; color: #374151;">Someone just confessed to you on <a href="https://confess.priyangsu.dev" style="color: #3B82F6; text-decoration: none;">confessions.priyangsu.dev</a></p>
+              <p style="font-size: 0.9rem; font-weight: 400; color: #374151;">Someone just confessed to you on <a href="https://confessions.priyangsu.dev" style="color: #3B82F6; text-decoration: none;">confessions.priyangsu.dev</a></p>
               <p style="font-size: 1rem; font-weight: 600; color: #374151;">${message}</p>
               <p style="font-size: 1rem; font-weight: 400; color: #374151;">
                <a href="https://ip-api.com/#${ip}" style="color: #3B82F6; text-decoration: none;">${ip}</a></p>
