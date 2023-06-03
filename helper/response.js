0adadbe517b4f2db6abaafc3a3035e19d2cd41cc
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-03 14:25:09 +0530

    response

--- helper/response.js
+++ helper/response.js
@@ -1,29 +1,29 @@
 const sendResponse = (message, ip) => {
-  fetch("https://onemail.vercel.app/api/send", {
-    method: "POST",
-    body: JSON.stringify({
-      secret: process.env.NEXT_PUBLIC_ONEMAIL_SECRET,
-      subject: "Confession received",
-      to: process.env.NEXT_PUBLIC_ONEMAIL_TO,
-      html: `
-            <div style="font-family: 'Inter', sans-serif;">
-              <h1 style="font-size: 1rem; font-weight: 600; color: #374151;">New confession received</h1>
-              <p style="font-size: 0.9rem; font-weight: 400; color: #374151;">Someone just confessed to you on <a href="https://confessions.priyangsu.dev" style="color: #3B82F6; text-decoration: none;">confessions.priyangsu.dev</a></p>
-              <p style="font-size: 1rem; font-weight: 600; color: #374151;">${message}</p>
-              <p style="font-size: 1rem; font-weight: 400; color: #374151;">
-               <a href="https://ipapi.co/${ip}/json" style="color: #3B82F6; text-decoration: none;">${ip}</a></p>
-              <p style="font-size: 1rem; font-weight: 400; color: #374151;">Have a great day!</p>
-              </div>
-    `,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-      "access-control-allow-origin": "*",
-    },
-  });
+  //   fetch("https://onemail.vercel.app/api/send", {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       secret: process.env.NEXT_PUBLIC_ONEMAIL_SECRET,
+  //       subject: "Confession received",
+  //       to: process.env.NEXT_PUBLIC_ONEMAIL_TO,
+  //       html: `
+  //             <div style="font-family: 'Inter', sans-serif;">
+  //               <h1 style="font-size: 1rem; font-weight: 600; color: #374151;">New confession received</h1>
+  //               <p style="font-size: 0.9rem; font-weight: 400; color: #374151;">Someone just confessed to you on <a href="https://confessions.priyangsu.dev" style="color: #3B82F6; text-decoration: none;">confessions.priyangsu.dev</a></p>
+  //               <p style="font-size: 1rem; font-weight: 600; color: #374151;">${message}</p>
+  //               <p style="font-size: 1rem; font-weight: 400; color: #374151;">
+  //                <a href="https://ipapi.co/${ip}/json" style="color: #3B82F6; text-decoration: none;">${ip}</a></p>
+  //               <p style="font-size: 1rem; font-weight: 400; color: #374151;">Have a great day!</p>
+  //               </div>
+  //     `,
+  //     }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "access-control-allow-origin": "*",
+  //     },
+  //   });
 
   let text = `
-      Received a new confession from: ${ip}%0A%0A${message}%0A%0Ahttps://ipapi.co/${ip}/json`;
+      Received a new confession%0A%0A${message}%0A%0Ahttps://ipapi.co/${ip}/json`;
 
   let urlString = `${process.env.NEXT_PUBLIC_TELEGRAM_URL}&text=${text}`;
   fetch(urlString, {
